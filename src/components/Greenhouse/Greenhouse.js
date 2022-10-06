254f0ee6d5bcc08bfcba209c352522ffae9fb3c6
alexander1fein <feinberg.alexander@gmail.com>
2022-10-06 16:10:14 -0400

    Phase1

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,17 +1,19 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
 
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
+import { useTheme } from "../../context/ThemeContext";
 
 function Greenhouse() {
-
+  const { themeName, setThemeName } = useTheme();
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
+      <img
+        className="greenhouse-img"
+        src={themeName === "day" ? dayImage : nightImage}
+        alt="greenhouse"
       />
       <LightSwitch />
       <ClimateStats />
